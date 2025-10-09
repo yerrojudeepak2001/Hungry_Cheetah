3f19e63dc66f34fe8b42e9e431d6fefa3c539b70
yerrojudeepak2001 <yerrojudeepak2001@gmail.com>
2025-10-09 15:58:44 -0500

    springboot version change

--- food-delivery-backend/order-service/src/main/java/com/foodapp/order/OrderServiceApplication.java
+++ food-delivery-backend/order-service/src/main/java/com/foodapp/order/OrderServiceApplication.java
@@ -3,6 +3,7 @@ package com.foodapp.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
